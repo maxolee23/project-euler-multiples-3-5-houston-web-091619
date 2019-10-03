c58622056a5fe7fe4b7e5bedcc75c4289175f429
Max Lee <maxolee23@gmail.com>
2019-10-03 16:58:46 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,7 +1,7 @@
 # Enter your procedural solution here!
 
 def collect_multiples(limit)
-  arr = Array(1..limit)
+  arr = Array(1...limit)
 
   multiples = arr.select do |num|
     num % 3 == 0 || num % 5 == 0 
