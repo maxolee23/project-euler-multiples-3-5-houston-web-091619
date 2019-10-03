7fceda837d15d6f4389f3b9b716a4c70e03f1127
Max Lee <maxolee23@gmail.com>
2019-10-03 17:03:23 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -6,5 +6,18 @@ class Multiples
   def initialize(limit)
     @limit = limit
   end
-  
+
+  def collect_multiples(limit)
+    arr = Array(1...limit)
+
+    multiples = arr.select do |num|
+      num % 3 == 0 || num % 5 == 0
+    end
+
+  end
+
+  def sum_multiples(limit)
+    collect_multiples(limit).sum
+  end
+
 end
