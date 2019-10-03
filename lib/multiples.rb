528fa6916c1631776a6138e7f623cd3b18b1e270
Max Lee <maxolee23@gmail.com>
2019-10-03 16:26:13 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,7 +1,7 @@
 # Enter your procedural solution here!
-@numbers = [3, 5, 6, 9]
+
 def collect_multiples(limit)
-  @numbers.map do |number|
-    number < 10
+  numbers = [3, 5, 6, 9]
+  numbers.collect {|x| x * x }
   end
 end
